bf7a6e83dc58fd4bace953265eac260acab5383f
jetrii <jetrii@gmail.com>
2013-06-08 15:40:18 -0400

    game framework changes

--- project-files/Rimatyo/src/com/jetllama/Rimatyo/PuzzleBase.java
+++ project-files/Rimatyo/src/com/jetllama/Rimatyo/PuzzleBase.java
@@ -1,7 +1,9 @@
 package com.jetllama.Rimatyo;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
 
 /**
@@ -10,21 +12,34 @@ import android.view.View;
 
 public class PuzzleBase extends View {
 
+
+    protected boolean localView;
     protected int CURRENT_STATE = 0;
 
 
     public PuzzleBase(Context context) {
         super(context);
+        Log.e(MainActivity.TAG, "PuzzleBase initialized without attrs");
     }
 
     public PuzzleBase(Context context, AttributeSet attrs) {
 
         super( context, attrs );
+        initialize(context, attrs);
     }
 
 
     public PuzzleBase(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
+        initialize(context, attrs);
+    }
+
+
+    protected void initialize(Context context, AttributeSet attrs) {
+
+        //Extract info passed through XML layout
+        TypedArray ta = context.obtainStyledAttributes(attrs, R.styleable.PuzzleBase);
+        localView = Boolean.parseBoolean(ta.getString(R.styleable.PuzzleBase_localView));
     }
 
     @Override
--- project-files/Rimatyo/src/com/jetllama/Rimatyo/PuzzleView.java
+++ project-files/Rimatyo/src/com/jetllama/Rimatyo/PuzzleView.java
@@ -28,32 +28,26 @@ public class PuzzleView extends PuzzleBase{
     }
 
 
-    protected void initialize(Context context) {
-        shapeArray = new Shape[5];
-
-        //Create 4 shapes + surprise shape
-        shapeArray[0] = new Shape(Color.RED, 0);
-        shapeArray[1] = new Shape(Color.GREEN, 1);
-        shapeArray[2] = new Shape(Color.CYAN, 2);
-        shapeArray[3] = new Shape(Color.YELLOW, 3);
-
-        shapeArray[4] = new Shape(Color.RED, 4);
-
-    }
-
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+
 
         int width = getWidth();
         int height = getHeight();
         Paint p = new Paint();
         p.setColor(Color.RED);
 
-        //Rect rect = new Rect(width/)
 
-        canvas.drawRect(new Rect(width/2,200, 500,500), p);
-        Log.d(MainActivity.TAG, "WIDTH: " + width/2);
+        canvas.drawRect(new Rect(width/4,height / 4, width/4 + width/5 ,height/4 + height/5 ), p);
+       // canvas.drawRect(new Rect(width/4 + width /4 ,height / 4, width/4 + width/5 ,height/4 + height/5 ), p);
+
+       // canvas.drawRect(new Rect(width/4,height / 4, width/4 + width/5 ,height/4 + height/5 ), p);
+       // canvas.drawRect(new Rect(width/4,height / 4, width/4 + width/5 ,height/4 + height/5 ), p);
+
+
+        //canvas.drawRect(new Rect(width/2,200, 500,500), p);
+        Log.d(MainActivity.TAG, "WIDTH: " + width / 2);
     }
 
 
@@ -70,6 +64,7 @@ public class PuzzleView extends PuzzleBase{
         }
 
         public void drawSelf(Canvas c){
+
 
         }
     }
